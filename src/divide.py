0a2fda55f876c49b853b73254a7dcd89c7c42617
Zaid <jaidmomin378@gmail.com>
2025-11-27 11:42:48 +0530

    Fixed default python Error message and added docstring

--- src/divide.py
+++ src/divide.py
@@ -3,10 +3,10 @@ def divide(a, b):
     '''
     Divides a by b and returns the result.
     If b is equals to zero then if block runs and it raises the ZeroDivision error 
-    and prints the message "Division by zero is not allowed".
+    and prints the message "Denominator cannot be zero.".
     '''
     
     if b == 0:
-        raise ZeroDivisionError("Division by zero is not allowed.") 
+        raise ZeroDivisionError("Denominator cannot be zero.") 
     
     return a / b
