c7d3e0b8e4ffa9b6e510fb2bce50c94b6576a112
Zaid <jaidmomin378@gmail.com>
2025-11-27 11:36:41 +0530

    Fixed the b = 0  for division also added docstring

--- src/divide.py
+++ src/divide.py
@@ -1,4 +1,12 @@
 def divide(a, b):
-    # Issue #6: Add docstring
-    # Issue #7: Fix divide() behavior for b = 0
+    
+    '''
+    Divides a by b and returns the result.
+    If b is equals to zero then if block runs and it raises the ZeroDivision error 
+    and prints the message "Division by zero is not allowed".
+    '''
+    
+    if b == 0:
+        raise ZeroDivisionError("Division by zero is not allowed.") 
+    
     return a / b
