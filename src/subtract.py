70be0be529b430f05c5ca7d436e344bb200987b7
Aman35256 <amanmishra35256@gmail.com>
2025-12-03 16:28:33 +0530

    Added docstring

--- src/subtract.py
+++ src/subtract.py
@@ -1,3 +1,11 @@
-def subtract(a, b):
-    # Issue #4: Add docstring
+def subtract(a: float, b: float) -> float:
+    """Return the result of subtracting b from a.
+
+    Args:
+        a (float): Minuend.
+        b (float): Subtrahend.
+
+    Returns:
+        float: The difference a - b.
+    """
     return a - b
